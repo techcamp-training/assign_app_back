409e192dc716b17c5da8ff8d2cf1003bc872a96f
sh10n7 <shion.yamauchi@di-v.co.jp>
2024-03-01 09:34:35 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+// cors対策
+const cors = require('cors')
+app.use(cors({
+  origin: 'http://localhost:8080',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
+
+
+// ポート3000を使用して、読み込みができるかテストする
+app.listen(3000, () => {
+  console.log("listening on localhost port 3000");
+})
