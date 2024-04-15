fc87aaf2ab20638c6775bb63e6affdf0334fd269
sh10n7 <shion.yamauchi@di-v.co.jp>
2024-04-15 11:19:57 +0900

    開発セットアップ

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.use(express.json());
 // cors対策
 const cors = require('cors')
 app.use(cors({
-  origin: 'http://localhost:8080',
+  origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
@@ -13,15 +13,6 @@ app.use(cors({
 // prismaの呼び出し
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
-
-// firebaseの初期化
-const admin = require("firebase-admin");
-var serviceAccount = require("./task-assign-app-key.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-
 
 // タスク保存処理の実装
 app.post('/task', async(req, res) => {
@@ -40,39 +31,6 @@ app.get('/tasks', async(_, res) => {
     res.json(tasks)
   }catch(error) {
     res.status(500).send("データの取得に失敗しました");
-  }
-})
-
-// userデータを取得するための記述。
-app.get('/users', async(req, res) => {
-  // レスポンス返却用の配列を準備
-  let allUsers = [];
-
-  // ユーザーのリストを取得するための非同期関数
-  const listAllUsers = async (nextPageToken) => {
-    try {
-      // Firebase Authenticationからユーザー情報を取得
-      const listUsersResult = await admin.auth().listUsers(1000, nextPageToken);
-      // 取得したユーザー情報を配列に追加
-      allUsers = allUsers.concat(listUsersResult.users.map(userRecord => userRecord.toJSON()));
-      // 次のページトークンがあれば、次のバッチのユーザーも取得
-      if (listUsersResult.pageToken) {
-        // 再帰的に次のバッチのユーザーを取得
-        await listAllUsers(listUsersResult.pageToken);
-      }
-    } catch (error) {
-      console.log('Error listing users:', error);
-      throw error; // エラーをスローして、後続の処理を停止
-    }
-  };
-
-  // リスト収集のメソッド呼び出し
-  try {
-    await listAllUsers();
-    console.log(allUsers)
-    res.json(allUsers);
-  }catch(error){
-    res.status(500).send("ユーザーリストの取得に失敗しました。");
   }
 })
 
